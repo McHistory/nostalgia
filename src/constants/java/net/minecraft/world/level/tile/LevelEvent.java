87eccf8c9cfeaaa0bb7ebb76cbb4c347b6ccc897
alpha <alphamodeyt@gmail.com>
2024-03-21 00:24:33 -0500

    Good mappings :ioa:

--- src/constants/java/net/minecraft/world/level/tile/LevelEvent.java
+++ src/constants/java/net/minecraft/world/level/tile/LevelEvent.java
@@ -1,11 +1,30 @@
 package net.minecraft.world.level.tile;
 
 public class LevelEvent {
+	/**
+	 * Data is always 0.
+	 */
 	public static final int SOUND_DISPENSER_DISPENSE = 1000;
+	/**
+	 * Data is always 0.
+	 */
 	public static final int SOUND_DISPENSER_FAIL = 1001;
+	/**
+	 * Data is always 0.
+	 */
 	public static final int SOUND_DISPENSER_PROJECTILE_LAUNCH = 1002;
+	/**
+	 * Data is always 0.
+	 */
 	public static final int SOUND_OPEN_OR_CLOSE = 1003;
-	public static final int LAVA_FIZZ = 1004;
+	/**
+	 * Data is always 0.
+	 */
+	public static final int SOUND_LAVA_FIZZ = 1004;
+	/**
+	 * Data is the item id of the record.
+	 */
+	public static final int SOUND_PLAY_RECORD = 1005;
 
 	public static final int PARTICLES_SHOOT_SMOKE = 2000;
 	public static final int PARTICLES_DESTROY_BLOCK = 2001;
