db2233fc4caa5fe35e23fa430dfd71037444a128
alpha <alphamodeyt@gmail.com>
2024-04-28 22:25:53 -0500

    Map and fix features

---
+++ src/constants/java/net/minecraft/network/protocol/game/GameEventType.java
@@ -0,0 +1,7 @@
+package net.minecraft.network.protocol.game;
+
+public class GameEventType {
+	public static final int NO_RESPAWN_TILE_AVAILABLE = 0;
+	public static final int START_RAINING = 1;
+	public static final int STOP_RAINING = 2;
+}
