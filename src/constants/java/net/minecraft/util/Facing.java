bce235431c1bcb13407b4490cc143dc5e44989ba
alpha <alphamodeyt@gmail.com>
2024-05-08 00:50:52 -0500

    More mapping

---
+++ src/constants/java/net/minecraft/util/Facing.java
@@ -0,0 +1,10 @@
+package net.minecraft.util;
+
+public final class Facing {
+	public static final int DOWN = 0;
+	public static final int UP = 1;
+	public static final int NORTH = 2;
+	public static final int SOUTH = 3;
+	public static final int WEST = 4;
+	public static final int EAST = 5;
+}
