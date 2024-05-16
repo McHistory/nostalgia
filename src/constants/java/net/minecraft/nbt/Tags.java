791cb151766d25c0d37bae07e1b77af57226a4f3
alpha <alphamodeyt@gmail.com>
2024-05-16 16:01:57 -0500

    Nbt unpick

---
+++ src/constants/java/net/minecraft/nbt/Tags.java
@@ -0,0 +1,19 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.nbt;
+
+public class Tags {
+	public static final byte TAG_END = 0;
+	public static final byte TAG_BYTE = 1;
+	public static final byte TAG_SHORT = 2;
+	public static final byte TAG_INT = 3;
+	public static final byte TAG_LONG = 4;
+	public static final byte TAG_FLOAT = 5;
+	public static final byte TAG_DOUBLE = 6;
+	public static final byte TAG_BYTE_ARRAY = 7;
+	public static final byte TAG_STRING = 8;
+	public static final byte TAG_LIST = 9;
+	public static final byte TAG_COMPOUND = 10;
+}
