c9c02f865108baaa2675b74ba2d30c7d8549197f
alpha <alphamodeyt@gmail.com>
2024-05-16 13:32:17 -0500

    Unpick block shapes and map there corresponding render methods

---
+++ src/constants/java/net/minecraft/client/renderer/BlockShapes.java
@@ -0,0 +1,27 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.client.renderer;
+
+public class BlockShapes {
+	public static final int NONE = -1;
+	public static final int BLOCK = 0;
+	public static final int REEDS = 1;
+	public static final int TORCH = 2;
+	public static final int FIRE = 3;
+	public static final int LIQUID = 4;
+	public static final int REDSTONE = 5;
+	public static final int CROP = 6;
+	public static final int DOOR = 7;
+	public static final int LADDER = 8;
+	public static final int RAILS = 9;
+	public static final int STAIRS = 10;
+	public static final int FENCE = 11;
+	public static final int LEVER = 12;
+	public static final int CACTUS = 13;
+	public static final int BED = 14;
+	public static final int REPEATER = 15;
+	public static final int PISTON = 16;
+	public static final int PISTON_HEAD = 17;
+}
