6622d9fd579c52e95268a5a8eb3de9b7b71de796
alpha <alphamodeyt@gmail.com>
2024-05-17 16:41:29 -0500

    add OpenScreenType (Maybe move this to a network constants class?)

---
+++ src/constants/java/net/minecraft/network/protocol/game/OpenScreenType.java
@@ -0,0 +1,12 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.network.protocol.game;
+
+public class OpenScreenType {
+	public static final int CONTAINER = 0;
+	public static final int CRAFTING = 1;
+	public static final int FURNACE = 2;
+	public static final int DISPENSER = 3;
+}
