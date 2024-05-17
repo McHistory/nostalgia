52be6472debb96efa84ec82d55073229f0ebe2f8
alpha <alphamodeyt@gmail.com>
2024-05-17 16:59:34 -0500

    Add unpick for Animate Packet and Interaction Packet

---
+++ src/constants/java/net/minecraft/network/protocol/game/AnimateType.java
@@ -0,0 +1,12 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.network.protocol.game;
+
+public class AnimateType {
+	public static final int SWING = 1;
+	public static final int ANIMATE_HURT = 2;
+	public static final int STOP_SLEEP = 3;
+	public static final int UNUSED = 4;
+}
---
+++ src/constants/java/net/minecraft/network/protocol/game/InteractType.java
@@ -0,0 +1,10 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.network.protocol.game;
+
+public class InteractType {
+	public static final int INTERACT_ON = 0;
+	public static final int ATTACK = 1;
+}
