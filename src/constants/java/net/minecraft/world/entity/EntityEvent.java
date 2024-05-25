af450eba5f09aaa0ef9ef6c9d8420db921e0f0b7
alpha <alphamodeyt@gmail.com>
2024-05-25 14:10:52 -0500

    EntityEvent unpick

---
+++ src/constants/java/net/minecraft/world/entity/EntityEvent.java
@@ -0,0 +1,16 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.world.entity;
+
+/**
+ *
+ */
+public class EntityEvent {
+	public static final byte DAMAGE = 2;
+	public static final byte DEATH = 3;
+	public static final byte TAMING_FAILED = 6;
+	public static final byte TAMING_SUCCEEDED = 7;
+	public static final byte SHAKE_WETNESS = 8;
+}
