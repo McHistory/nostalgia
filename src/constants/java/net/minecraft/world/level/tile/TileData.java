24eacfaf5a071d93aea65ea9b055b37090450037
alpha <alphamodeyt@gmail.com>
2024-05-31 02:13:18 -0500

    TileData unpick and more misc mappings

---
+++ src/constants/java/net/minecraft/world/level/tile/TileData.java
@@ -0,0 +1,12 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.world.level.tile;
+
+public class TileData {
+	// Bed
+	public static final byte BED_ORIENTATION_MASK = 3;
+	public static final byte BED_OCCUPIED_FLAG = 4;
+	public static final byte BED_HEAD_PIECE_FLAG = 8;
+}
