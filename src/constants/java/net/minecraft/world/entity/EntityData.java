fa7d3b9c72709a1ad15f4eeeafe66a11eb7729f3
alpha <alphamodeyt@gmail.com>
2024-06-06 11:02:45 -0500

    Entity flag and data unpick

---
+++ src/constants/java/net/minecraft/world/entity/EntityData.java
@@ -0,0 +1,17 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.world.entity;
+
+public final class EntityData {
+	public static final int DATA_FLAGS = 0;
+	public static final int DATA_PIG_SADDLE = 16;
+	public static final int DATA_GHAST_IS_CHARGING= 16;
+	public static final int DATA_SHEEP_WOOL_ID = 16;
+	public static final int DATA_SLIME_ID_SIZE = 16;
+
+	public static final int FLAG_ONFIRE = 0;
+	public static final int FLAG_SHIFT_KEY_DOWN = 1;
+	public static final int FLAG_HAS_VEHICLE = 2;
+}
