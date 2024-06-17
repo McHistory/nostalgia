5cdc9780bb12f1458e2dd3341f26f1aaa3b049a4
alpha <alphamodeyt@gmail.com>
2024-06-17 01:12:20 -0500

    Fix space in EntityData

--- src/constants/java/net/minecraft/world/entity/EntityData.java
+++ src/constants/java/net/minecraft/world/entity/EntityData.java
@@ -7,7 +7,7 @@ package net.minecraft.world.entity;
 public final class EntityData {
 	public static final int DATA_FLAGS = 0;
 	public static final int DATA_PIG_SADDLE = 16;
-	public static final int DATA_GHAST_IS_CHARGING= 16;
+	public static final int DATA_GHAST_IS_CHARGING = 16;
 	public static final int DATA_SHEEP_WOOL_ID = 16;
 	public static final int DATA_SLIME_ID_SIZE = 16;
 
