ad90d97d5e9ed61fe6ef0d218c930f195d3c380b
alpha <alphamodeyt@gmail.com>
2024-06-27 15:00:43 -0500

    Better opengl unpick

---
+++ src/constants/java/net/minecraft/client/renderer/GlConst.java
@@ -0,0 +1,13 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+package net.minecraft.client.renderer;
+
+public class GlConst {
+	public static final int
+			GL_COLOR_BUFFER_BIT    = 0x4000,
+			GL_DEPTH_BUFFER_BIT    = 0x100;
+
+	public static final int GL_COLOR_AND_DEPTH_BUFFER_BIT = GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT;
+}
