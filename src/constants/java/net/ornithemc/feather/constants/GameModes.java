cd8ced4ab29331d4774085e66de40fb32437fe0e
Space Walker <spacedoesrs@gmail.com>
2025-05-06 20:22:17 +0200

    unpick definitions for game modes

---
+++ src/constants/java/net/ornithemc/feather/constants/GameModes.java
@@ -0,0 +1,9 @@
+package net.ornithemc.feather.constants;
+
+public class GameModes {
+
+	public static final int NOT_SET  = -1;
+	public static final int SURVIVAL = 0;
+	public static final int CREATIVE = 1;
+
+}
