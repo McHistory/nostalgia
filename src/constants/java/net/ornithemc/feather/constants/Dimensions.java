09a46838a34bff93501234309e65ff2462ea3989
Space Walker <spacedoesrs@gmail.com>
2025-05-07 20:41:57 +0200

    unpick definitions for dimensions

---
+++ src/constants/java/net/ornithemc/feather/constants/Dimensions.java
@@ -0,0 +1,9 @@
+package net.ornithemc.feather.constants;
+
+public class Dimensions {
+
+	public static final int NETHER    = -1;
+	public static final int OVERWORLD = 0;
+	public static final int THE_END   = 1;
+
+}
