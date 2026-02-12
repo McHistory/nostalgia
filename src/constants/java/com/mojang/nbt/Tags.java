dfa3de2118c8c6bb7dbd63b4f0bfde1860a3f054
alpha <alphamodeyt@gmail.com>
2026-02-12 17:05:13 -0600

    Unpick v4

--- src/constants/java/com/mojang/nbt/Tags.java
+++ src/constants/java/com/mojang/nbt/Tags.java
@@ -2,7 +2,7 @@
  * This file is free for everyone to use under the Creative Commons Zero license.
  */
 
-package net.minecraft.nbt;
+package com.mojang.nbt;
 
 public class Tags {
 	public static final byte TAG_END = 0;
