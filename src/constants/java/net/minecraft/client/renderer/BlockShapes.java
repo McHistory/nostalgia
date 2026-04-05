33240c72da2ad2eae5148d080923228e7498d989
alpha <alphamodeyt@gmail.com>
2026-04-04 22:17:58 -0500

    More b1.8 mappings

--- src/constants/java/net/minecraft/client/renderer/BlockShapes.java
+++ src/constants/java/net/minecraft/client/renderer/BlockShapes.java
@@ -5,23 +5,28 @@
 package net.minecraft.client.renderer;
 
 public class BlockShapes {
-	public static final int NONE = -1;
-	public static final int BLOCK = 0;
-	public static final int REEDS = 1;
-	public static final int TORCH = 2;
-	public static final int FIRE = 3;
-	public static final int LIQUID = 4;
-	public static final int REDSTONE = 5;
-	public static final int CROP = 6;
-	public static final int DOOR = 7;
-	public static final int LADDER = 8;
-	public static final int RAILS = 9;
-	public static final int STAIRS = 10;
-	public static final int FENCE = 11;
-	public static final int LEVER = 12;
-	public static final int CACTUS = 13;
-	public static final int BED = 14;
-	public static final int REPEATER = 15;
-	public static final int PISTON = 16;
-	public static final int PISTON_HEAD = 17;
+	public static final int SHAPE_INVISIBLE = -1;
+	public static final int SHAPE_BLOCK = 0;
+	public static final int SHAPE_CROSS_TEXTURE = 1;
+	public static final int SHAPE_TORCH = 2;
+	public static final int SHAPE_FIRE = 3;
+	public static final int SHAPE_WATER = 4;
+	public static final int SHAPE_RED_DUST = 5;
+	public static final int SHAPE_ROWS = 6;
+	public static final int SHAPE_DOOR = 7;
+	public static final int SHAPE_LADDER = 8;
+	public static final int SHAPE_RAIL = 9;
+	public static final int SHAPE_STAIRS = 10;
+	public static final int SHAPE_FENCE = 11;
+	public static final int SHAPE_LEVER = 12;
+	public static final int SHAPE_CACTUS = 13;
+	public static final int SHAPE_BED = 14;
+	public static final int SHAPE_REPEATER = 15;
+	public static final int SHAPE_PISTON_BASE = 16;
+	public static final int SHAPE_PISTON_EXTENSION = 17;
+	public static final int SHAPE_IRON_FENCE = 18;
+	public static final int SHAPE_STEM = 19;
+	public static final int SHAPE_VINE = 20;
+	public static final int SHAPE_FENCE_GATE = 21;
+	public static final int SHAPE_ENTITYTILE_ANIMATED = 22;
 }
